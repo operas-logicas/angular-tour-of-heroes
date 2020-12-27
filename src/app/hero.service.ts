58ccbd49c275e30ebeebde024da0439e4c0c1901
Robert Miller <miller.robert.john@gmail.com>
2020-12-27 16:55:19 -0700

    Added DashboardComponent and AppRoutingModule

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
@@ -10,6 +11,12 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: Fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
